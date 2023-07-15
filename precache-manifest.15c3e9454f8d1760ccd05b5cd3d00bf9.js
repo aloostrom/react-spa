cce790d66eafbd734173ee3ebbeaa9645507d5e8
aloostrom <aloostrom@gmail.com>
2023-07-15 17:05:48 -0400

    Updates

---
+++ precache-manifest.15c3e9454f8d1760ccd05b5cd3d00bf9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d0fb550f7a8d16a98cbee7c23865e2",
+    "url": "/react-spa/index.html"
+  },
+  {
+    "revision": "0e805f031f382d0e0b6a",
+    "url": "/react-spa/static/css/main.e8433e66.chunk.css"
+  },
+  {
+    "revision": "77e1f6b59b456523f6cb",
+    "url": "/react-spa/static/js/2.5f9ec20e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-spa/static/js/2.5f9ec20e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e805f031f382d0e0b6a",
+    "url": "/react-spa/static/js/main.ffd8b5d8.chunk.js"
+  },
+  {
+    "revision": "0d7845493fe936b3f114",
+    "url": "/react-spa/static/js/runtime-main.1586cd94.js"
+  },
+  {
+    "revision": "634847fab37b34d3867b6dcc00691717",
+    "url": "/react-spa/static/media/bmw8er.634847fa.png"
+  },
+  {
+    "revision": "bcac43283ef2ebb7d77b74cf5dd798e3",
+    "url": "/react-spa/static/media/fusion.bcac4328.png"
+  },
+  {
+    "revision": "a4daa9bcffcfade5c9e7adb2f1670903",
+    "url": "/react-spa/static/media/homeImage.a4daa9bc.png"
+  },
+  {
+    "revision": "fe3d2082a01a7d38fd0fd8618cc97ab2",
+    "url": "/react-spa/static/media/rav4.fe3d2082.png"
+  },
+  {
+    "revision": "0b1892c2f0a21d05194e463c31b4fe76",
+    "url": "/react-spa/static/media/shelby-3821716_1920.0b1892c2.png"
+  },
+  {
+    "revision": "2be8d19260be895effe89eae8cf4b246",
+    "url": "/react-spa/static/media/silverado.2be8d192.png"
+  }
+]);
